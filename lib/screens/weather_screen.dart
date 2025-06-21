68d6273daa28a2927863aa707fc5620b3d951fac
migarakavishan <migarakavishan43@gmail.com>
2025-06-21 22:10:16 +0530

    Add WeatherInfo widget to display detailed weather data in WeatherScreen

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -4,6 +4,7 @@ import 'package:weather_app/constants/text_styles.dart';
 import 'package:weather_app/extensions/datetime.dart';
 import 'package:weather_app/providers/current_weather_provider.dart';
 import 'package:weather_app/views/gradient_container.dart';
+import 'package:weather_app/views/weather_info.dart';
 
 class WeatherScreen extends ConsumerWidget {
   const WeatherScreen({super.key});
@@ -17,18 +18,27 @@ class WeatherScreen extends ConsumerWidget {
         return GradientContainer(
           children: [
             SizedBox(width: double.infinity),
-            Text(weather.name, style: TextStyles.h1),
-            SizedBox(height: 20),
-            Text(DateTime.now().dateTime),
-            SizedBox(height: 30),
-            SizedBox(
-              height: 260,
-              child: Image.asset(
-                'assets/icons/${weather.weather[0].icon.replaceAll('n', 'd')}.png',
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(weather.name, style: TextStyles.h1),
+                SizedBox(height: 20),
+                Text(DateTime.now().dateTime, style: TextStyles.subtitleText,),
+                SizedBox(height: 30),
+                SizedBox(
+                  height: 260,
+                  child: Image.asset(
+                    'assets/icons/${weather.weather[0].icon.replaceAll('n', 'd')}.png',
+                  ),
+                ),
+                SizedBox(height: 40),
+                Text(weather.weather[0].description, style: TextStyles.h2),
+                SizedBox(height: 40),
+                WeatherInfo(
+                  weather: weather,
+                ),
+              ],
             ),
-            SizedBox(height: 40),
-            Text(weather.weather[0].description, style: TextStyles.h2),
           ],
         );
       },
