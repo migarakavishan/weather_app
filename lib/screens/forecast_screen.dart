2a84ec643ee341dff643d4aa655670efd0f68e73
migarakavishan <migarakavishan43@gmail.com>
2025-08-29 13:02:46 +0530

    Add ForecastScreen and update HomeScreen navigation to include it

---
+++ lib/screens/forecast_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/constants/text_styles.dart';
+import 'package:weather_app/extensions/datetime.dart';
+import 'package:weather_app/views/gradient_container.dart';
+import 'package:weather_app/views/hourly_forecast_view.dart';
+
+class ForecastScreen extends StatelessWidget {
+  const ForecastScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GradientContainer(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Text('Forecast Report', style: TextStyles.h1),
+          ),
+          SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Today', style: TextStyles.h2),
+              Text(DateTime.now().dateTime, style: TextStyles.subtitleText),
+            ],
+          ),
+          SizedBox(height: 20),
+          HourlyForecastView(),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Next Forecast', style: TextStyles.h2),
+              Icon(Icons.calendar_month_outlined, color: Colors.white),
+            ],
+          ),
+          SizedBox(height: 20),
+
+          WeeklyForecastView(),
+        ],
+      ),
+    );
+  }
+}
